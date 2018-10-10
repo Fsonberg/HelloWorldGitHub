d0b88b499652d869718cf212a22f36da378bb0ad
Rasmus Lohse <rnlohse@ruc.dk>
2018-10-10 13:15:15 +0200

    new comment

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hej");
+        // Do you have any spicy new memes?
     }
+
 }
